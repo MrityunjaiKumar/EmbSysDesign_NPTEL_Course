7ab35357148d93327e02a9cb14c750a035de0587
divanshudodeja <divanshu.dodeja@gmail.com>
2020-01-21 03:42:10 +0530

    Exp00_LunchBox_Template

---
+++ Exp00_LunchBox_Template/Exp00_LunchBox_Template.c
@@ -0,0 +1,13 @@
+#include <msp430.h> 
+
+
+
+/*@brief entry point for the code*/
+int main(void)
+{
+	WDTCTL = WDTPW | WDTHOLD;	//! Stop Watchdog (Not recommended for code in production and devices working in field)
+
+	while(1){
+
+	}
+}
