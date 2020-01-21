f3f816618eb9376df5d84ab28349b10a521de8dc
divanshudodeja <divanshu.dodeja@gmail.com>
2020-01-21 19:00:01 +0530

    Exp03_LunchBox_HelloSwitch

---
+++ Software/Examples_Msp430G2553_LunchBox/Exp03_LunchBox_HelloSwitch/Exp03_LunchBox_HelloSwitch.c
@@ -0,0 +1,21 @@
+#include <msp430.h> 
+
+#define SW  BIT3                    // Switch -> P1.3
+#define LED BIT0                    // Red LED -> P1.0
+
+void main(void) {
+    WDTCTL = WDTPW | WDTHOLD;       // Stop watchdog timer
+
+    P1DIR |= LED;                   // Set LED pin -> Output
+    P1DIR &= ~SW;                   // Set SW pin -> Input
+    P1REN |= SW;                    // Enable Resistor for SW pin
+    P1OUT |= SW;                    // Select Pull Up for SW pin
+
+    while(1)
+    {
+        if(!(P1IN & SW))            // If SW is Pressed
+        {
+            P1OUT ^= LED;           // Toggle LED
+        }
+    }
+}
