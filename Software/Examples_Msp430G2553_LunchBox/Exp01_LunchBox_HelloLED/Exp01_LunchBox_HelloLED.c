79a38ac3f795b851ca538c54a0d170a1e2df686a
divanshudodeja <divanshu.dodeja@gmail.com>
2020-01-25 19:49:14 +0530

    Updated Exp01_LunchBox_HelloLED.c according to LunchBox LED

--- Software/Examples_Msp430G2553_LunchBox/Exp01_LunchBox_HelloLED/Exp01_LunchBox_HelloLED.c
+++ Software/Examples_Msp430G2553_LunchBox/Exp01_LunchBox_HelloLED/Exp01_LunchBox_HelloLED.c
@@ -3,9 +3,12 @@
 /*@brief entry point for the code*/
 int main(void) {
     WDTCTL = WDTPW | WDTHOLD;       //! Stop Watchdog (Not recommended for code in production and devices working in field)
-    P1DIR |= 0x01;                  // Set P1.0 to output direction
 
-    P1OUT |= 0x01;                  // Toggle P1.0 using exclusive-OR
+    //P1DIR |= BIT7;
+    P1DIR |= 0x80;                  // Set P1.7 to output direction
+
+    //P1OUT |= BIT7;
+    P1OUT |= 0x80;                  // Set P1.7 to HIGH voltage
 
     return 0;
 }
