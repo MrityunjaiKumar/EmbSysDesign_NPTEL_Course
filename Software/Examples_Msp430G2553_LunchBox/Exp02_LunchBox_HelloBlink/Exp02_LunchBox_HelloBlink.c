e303e5355ef0e21e7d8afb5c422b29e45b640e06
divanshudodeja <divanshu.dodeja@gmail.com>
2020-01-25 19:54:05 +0530

    Updated Exp02_LunchBox_HelloBlink.c according to LunchBox LED

--- Software/Examples_Msp430G2553_LunchBox/Exp02_LunchBox_HelloBlink/Exp02_LunchBox_HelloBlink.c
+++ Software/Examples_Msp430G2553_LunchBox/Exp02_LunchBox_HelloBlink/Exp02_LunchBox_HelloBlink.c
@@ -1,22 +1,24 @@
 #include <msp430.h> 
 
 
-
 /*@brief entry point for the code*/
 int main(void)
 {
 	WDTCTL = WDTPW | WDTHOLD;	//! Stop Watchdog (Not recommended for code in production and devices working in field)
 
-	P1DIR |= BIT7;            // P1.0 (Red LED)
+	//P1DIR |= BIT7;
+	P1DIR |= 0x80;            // P1.7 (Red LED)
 
     while(1)
     {
         volatile unsigned long i;
 
-        P1OUT |= BIT7;              //Red LED -> ON
+        //P1OUT |= BIT7;
+        P1OUT |= 0x80;              //Red LED -> ON
         for(i = 0; i<10000; i++);   //delay
 
-        P1OUT &= ~BIT7;             //Red LED -> OFF
+        //P1OUT &=~ BIT7;
+        P1OUT &= ~0x80;             //Red LED -> OFF
         for(i = 0; i<10000; i++);   //delay
     }
 }
