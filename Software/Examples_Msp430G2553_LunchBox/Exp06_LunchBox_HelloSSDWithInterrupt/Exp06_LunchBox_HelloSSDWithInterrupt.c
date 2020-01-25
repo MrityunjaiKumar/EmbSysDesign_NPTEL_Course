f8b2d178927997f2bd786ab9abaeda44f404a21e
divanshudodeja <divanshu.dodeja@gmail.com>
2020-01-25 21:13:03 +0530

    Updated Exp05_LunchBox_HelloSSDWithInterrupt.c, changes left

--- Software/Examples_Msp430G2553_LunchBox/Exp06_LunchBox_HelloSSDWithInterrupt/Exp06_LunchBox_HelloSSDWithInterrupt.c
+++ Software/Examples_Msp430G2553_LunchBox/Exp06_LunchBox_HelloSSDWithInterrupt/Exp06_LunchBox_HelloSSDWithInterrupt.c
@@ -1,27 +1,70 @@
 #include <msp430.h> 
 
-#define SW      BIT3                    // Switch -> P1.3
-#define GREEN   BIT6                    // Green LED -> P1.6
+#define SW      BIT3                    // Switch -> P2.3
+
+// Define Pin Mapping of 7-segment Display
+// Segments are connected to P1.0 - P1.7
+#define SEG_A   BIT0
+#define SEG_B   BIT1
+#define SEG_C   BIT2
+#define SEG_D   BIT3
+#define SEG_E   BIT4
+#define SEG_F   BIT5
+#define SEG_G   BIT6
+#define SEG_DP  BIT7
+
+// Define each digit according to truth table
+#define D0  (SEG_A + SEG_B + SEG_C + SEG_D + SEG_E + SEG_F)
+#define D1  (SEG_B + SEG_C)
+#define D2  (SEG_A + SEG_B + SEG_D + SEG_E + SEG_G)
+#define D3  (SEG_A + SEG_B + SEG_C + SEG_D + SEG_G)
+#define D4  (SEG_B + SEG_C + SEG_F + SEG_G)
+#define D5  (SEG_A + SEG_C + SEG_D + SEG_F + SEG_G)
+#define D6  (SEG_A + SEG_C + SEG_D + SEG_E + SEG_F + SEG_G)
+#define D7  (SEG_A + SEG_B + SEG_C)
+#define D8  (SEG_A + SEG_B + SEG_C + SEG_D + SEG_E + SEG_F + SEG_G)
+#define D9  (SEG_A + SEG_B + SEG_C + SEG_D + SEG_F + SEG_G)
+#define DA  (SEG_A + SEG_B + SEG_C + SEG_D + SEG_F + SEG_G)
+#define DB  (SEG_A + SEG_B + SEG_C + SEG_D + SEG_F + SEG_G)
+#define DC  (SEG_A + SEG_B + SEG_C + SEG_D + SEG_F + SEG_G)
+#define DD  (SEG_A + SEG_B + SEG_C + SEG_D + SEG_F + SEG_G)
+#define DE  (SEG_A + SEG_B + SEG_C + SEG_D + SEG_F + SEG_G)
+#define DF  (SEG_A + SEG_B + SEG_C + SEG_D + SEG_F + SEG_G)
+
+
+// Define mask value for all digit segments except DP
+#define DMASK   ~(SEG_A + SEG_B + SEG_C + SEG_D + SEG_E + SEG_F + SEG_G)
+
+// Store digits in array for display
+const unsigned int digits[10] = {D0, D1, D2, D3, D4, D5, D6, D7, D8, D9, DA, DB, DC, DD, DE, DF};
+
+volatile unsigned int i = 0;
 
 void main(void) {
     WDTCTL = WDTPW | WDTHOLD;           // Stop watchdog timer
 
-    P1DIR |= GREEN;                     // Set LED pin -> Output
-    P1DIR &= ~SW;                       // Set SW pin -> Input
-    P1REN |= SW;                        // Enable Resistor for SW pin
-    P1OUT |= SW;                        // Select Pull Up for SW pin
+    // Initialize 7-segment pins as Output
+    P1DIR |= (SEG_A + SEG_B + SEG_C + SEG_D + SEG_E+ SEG_F + SEG_G + SEG_DP);
 
-    P1IES &= ~SW;                       // Select Interrupt on Rising Edge
-    P1IE |= SW;                         // Enable Interrupt on SW pin
 
-    __bis_SR_register(GIE); // Enter LPM4 and Enable CPU Interrupt
+    P2DIR &= ~SW;                       // Set SW pin -> Input
+    P2REN |= SW;                        // Enable Resistor for SW pin
+    P2OUT |= SW;                        // Select Pull Up for SW pin
 
-    while(1);
+    P2IES &= ~SW;                       // Select Interrupt on Rising Edge
+    P2IE |= SW;                         // Enable Interrupt on SW pin
+
+    while(1)
+    {
+        P1OUT = (P2OUT & DMASK) + digits[i];    // Display current digit
+    }
 }
 
-#pragma vector=PORT1_VECTOR
-__interrupt void Port_1(void)
+#pragma vector=PORT2_VECTOR
+__interrupt void Port_2(void)
 {
-    P1OUT ^= GREEN;                     // Toggle Green LED
-    P1IFG &= ~SW;                       // Clear SW interrupt flag
+    i++;
+    if(i > 15)
+        i = 0;
+    P2IFG &= ~SW;                       // Clear SW interrupt flag
 }
