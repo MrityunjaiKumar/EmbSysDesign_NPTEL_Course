60b1a0949afd3b9be9bf162f4a6af09e4df6c516
divanshudodeja <divanshu.dodeja@gmail.com>
2020-01-25 20:51:13 +0530

    Updated Exp05_LunchBox_HelloInterrupt.c according to LunchBox LED and Switch

--- Software/Examples_Msp430G2553_LunchBox/Exp05_LunchBox_HelloInterrupt/Exp05_LunchBox_HelloInterrupt.c
+++ Software/Examples_Msp430G2553_LunchBox/Exp05_LunchBox_HelloInterrupt/Exp05_LunchBox_HelloInterrupt.c
@@ -1,12 +1,12 @@
 #include <msp430.h> 
 
 #define SW      BIT3                    // Switch -> P1.3
-#define GREEN   BIT6                    // Green LED -> P1.6
+#define RED     BIT7                    // Green LED -> P1.7
 
 void main(void) {
     WDTCTL = WDTPW | WDTHOLD;           // Stop watchdog timer
 
-    P1DIR |= GREEN;                     // Set LED pin -> Output
+    P1DIR |= RED;                       // Set LED pin -> Output
     P1DIR &= ~SW;                       // Set SW pin -> Input
     P1REN |= SW;                        // Enable Resistor for SW pin
     P1OUT |= SW;                        // Select Pull Up for SW pin
@@ -14,7 +14,7 @@ void main(void) {
     P1IES &= ~SW;                       // Select Interrupt on Rising Edge
     P1IE |= SW;                         // Enable Interrupt on SW pin
 
-    __bis_SR_register(GIE); // Enter LPM4 and Enable CPU Interrupt
+    __bis_SR_register(GIE);             // Enable CPU Interrupt
 
     while(1);
 }
@@ -22,6 +22,6 @@ void main(void) {
 #pragma vector=PORT1_VECTOR
 __interrupt void Port_1(void)
 {
-    P1OUT ^= GREEN;                     // Toggle Green LED
+    P1OUT ^= RED;                     // Toggle Green LED
     P1IFG &= ~SW;                       // Clear SW interrupt flag
 }
