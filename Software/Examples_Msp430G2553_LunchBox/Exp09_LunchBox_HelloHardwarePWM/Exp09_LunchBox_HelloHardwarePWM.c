006dfb290fdaf4c0509762b9705a9a7146f7cae0
divanshudodeja <divanshu.dodeja@gmail.com>
2020-01-29 15:45:44 +0530

    Exp09_LunchBox_HelloHardwarePWM final commit

--- Software/Examples_Msp430G2553_LunchBox/Exp09_LunchBox_HelloHardwarePWM/Exp09_LunchBox_HelloHardwarePWM.c
+++ Software/Examples_Msp430G2553_LunchBox/Exp09_LunchBox_HelloHardwarePWM/Exp09_LunchBox_HelloHardwarePWM.c
@@ -1,27 +1,53 @@
 #include <msp430.h> 
 
-#define SW      BIT3                    // Switch -> P1.3
-#define GREEN   BIT6                    // Green LED -> P1.6
+#define SW  BIT3                            // Switch -> P1.3
+#define GREEN   BIT6                        // Green LED -> P1.6
 
-void main(void) {
-    WDTCTL = WDTPW | WDTHOLD;           // Stop watchdog timer
+/**
+ * @brief
+ * These settings are wrt enabling TIMER0 on Lunchbox
+ **/
+void register_settings_for_TIMER0()
+{
+    P1DIR |= GREEN;                         // Green LED -> Output
+    P1SEL |= GREEN;                         // Green LED -> Select Timer Output
 
-    P1DIR |= GREEN;                     // Set LED pin -> Output
-    P1DIR &= ~SW;                       // Set SW pin -> Input
-    P1REN |= SW;                        // Enable Resistor for SW pin
-    P1OUT |= SW;                        // Select Pull Up for SW pin
-
-    P1IES &= ~SW;                       // Select Interrupt on Rising Edge
-    P1IE |= SW;                         // Enable Interrupt on SW pin
-
-    __bis_SR_register(GIE); // Enter LPM4 and Enable CPU Interrupt
-
-    while(1);
+    CCR0 = 20000;                             // Set Timer0 PWM Period
+    CCTL1 = OUTMOD_7;                       // Set TA0.1 Waveform Mode - Clear on Compare, Set on Overflow
+    CCR1 = 1;                               // Set TA0.1 PWM duty cycle
+    TACTL = TASSEL_2 + MC_1 + ID_3;                // Timer Clock -> SMCLK, Mode -> Up Count
 }
 
-#pragma vector=PORT1_VECTOR
-__interrupt void Port_1(void)
-{
-    P1OUT ^= GREEN;                     // Toggle Green LED
-    P1IFG &= ~SW;                       // Clear SW interrupt flag
+/*@brief entry point for the code*/
+void main(void) {
+    unsigned int state = 0;
+
+    WDTCTL = WDTPW | WDTHOLD;               // Stop watchdog timer
+
+    P1DIR &= ~SW;                           // Set SW pin -> Input
+    P1REN |= SW;                            // Enable Resistor for SW pin
+    P1OUT |= SW;                            // Select Pull Up for SW pin
+
+    register_settings_for_TIMER0();
+
+    while(1)
+    {
+        if(!(P1IN & SW))                    // If SW is Pressed
+            {
+                __delay_cycles(20000);      // Wait 20ms to debounce
+                while(!(P1IN & SW));        // Wait till SW Released
+                if(state == 0)
+                {
+                    CCR1 = CCR1 + 1500;       // Increase Duty from min to max
+                    if(CCR1 > 20000)
+                        state = 1;
+                }
+                else if(state == 1)
+                {
+                    CCR1 = CCR1 - 1500;       // Increase Duty from min to max
+                    if(CCR1 < 100)
+                        state = 0;
+                }
+            }
+    }
 }
