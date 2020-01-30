59f49f32aa00cd7142af82730fac6d3cfe93f695
divanshudodeja <divanshu.dodeja@gmail.com>
2020-01-30 16:29:49 +0530

    Exp07_LunchBox_HelloLPM final commit

--- Software/Examples_Msp430G2553_LunchBox/Exp07_LunchBox_HelloLPM/Exp07_LunchBox_HelloLPM.c
+++ Software/Examples_Msp430G2553_LunchBox/Exp07_LunchBox_HelloLPM/Exp07_LunchBox_HelloLPM.c
@@ -1,12 +1,13 @@
 #include <msp430.h> 
 
 #define SW      BIT3                    // Switch -> P1.3
-#define GREEN   BIT6                    // Green LED -> P1.6
+#define RED     BIT7                      // Green LED -> P1.7
 
+/*@brief entry point for the code*/
 void main(void) {
     WDTCTL = WDTPW | WDTHOLD;           // Stop watchdog timer
 
-    P1DIR |= GREEN;                     // Set LED pin -> Output
+    P1DIR |= RED;                       // Set LED pin -> Output
     P1DIR &= ~SW;                       // Set SW pin -> Input
     P1REN |= SW;                        // Enable Resistor for SW pin
     P1OUT |= SW;                        // Select Pull Up for SW pin
@@ -14,14 +15,19 @@ void main(void) {
     P1IES &= ~SW;                       // Select Interrupt on Rising Edge
     P1IE |= SW;                         // Enable Interrupt on SW pin
 
-    __bis_SR_register(GIE); // Enter LPM4 and Enable CPU Interrupt
+    while(1)
+    {
+        //__bis_SR_register(GIE);             // Enable CPU Interrupt
+        __bis_SR_register(LPM4_bits + GIE); // Enter LPM4 and Enable CPU Interrupt
 
-    while(1);
+        P1OUT ^= RED;                       // Toggle Green LED
+    }
 }
 
+/*@brief entry point for switch interrupt*/
 #pragma vector=PORT1_VECTOR
 __interrupt void Port_1(void)
 {
-    P1OUT ^= GREEN;                     // Toggle Green LED
-    P1IFG &= ~SW;                       // Clear SW interrupt flag
+    __bic_SR_register_on_exit(LPM4_bits + GIE);     // Exit LPM0 on return to main
+    P1IFG &= ~SW;                                   // Clear SW interrupt flag
 }
