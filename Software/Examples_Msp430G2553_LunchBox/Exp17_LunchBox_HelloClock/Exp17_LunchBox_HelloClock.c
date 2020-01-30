eedf197f0d4f5104fc2436b9168fc0ce528c440d
divanshudodeja <divanshu.dodeja@gmail.com>
2020-01-30 17:20:43 +0530

    Exp17_LunchBox_HelloClock final commit

--- Software/Examples_Msp430G2553_LunchBox/Exp17_LunchBox_HelloClock/Exp17_LunchBox_HelloClock.c
+++ Software/Examples_Msp430G2553_LunchBox/Exp17_LunchBox_HelloClock/Exp17_LunchBox_HelloClock.c
@@ -3,10 +3,48 @@
 #define SW      BIT3                    // Switch -> P1.3
 #define GREEN   BIT6                    // Green LED -> P1.6
 
+
+#define SMCLK   0
+#define ACLK    1
+
+unsigned int clockSource = 1;
+
+/**
+ * @brief
+ * These settings are wrt enabling TIMER0 on Lunchbox
+ **/
+void register_settings_for_TIMER0()
+{
+    P1DIR |= GREEN;                         // Green LED -> Output
+    P1SEL |= GREEN;                         // Green LED -> Select Timer Output
+
+    CCR0 = 20000;                             // Set Timer0 PWM Period
+    CCTL1 = OUTMOD_7;                       // Set TA0.1 Waveform Mode - Clear on Compare, Set on Overflow
+    CCR1 = 10000;                               // Set TA0.1 PWM duty cycle
+}
+
+/**
+ * @brief
+ * These settings are wrt enabling TIMER0 wrt SMCLK on Lunchbox
+ **/
+void register_settings_for_TIMER0_with_SMCLK()
+{
+   TACTL = TASSEL_2 + MC_1 + ID_3;                // Timer Clock -> SMCLK, Mode -> Up Count
+}
+
+/**
+ * @brief
+ * These settings are wrt enabling TIMER0 wrt ACLK on Lunchbox
+ **/
+void register_settings_for_TIMER0_with_ACLK()
+{
+    TACTL = TASSEL_1 + MC_1 + ID_3;                // Timer Clock -> SMCLK, Mode -> Up Count
+}
+
+/*@brief entry point for the code*/
 void main(void) {
     WDTCTL = WDTPW | WDTHOLD;           // Stop watchdog timer
 
-    P1DIR |= GREEN;                     // Set LED pin -> Output
     P1DIR &= ~SW;                       // Set SW pin -> Input
     P1REN |= SW;                        // Enable Resistor for SW pin
     P1OUT |= SW;                        // Select Pull Up for SW pin
@@ -14,14 +52,27 @@ void main(void) {
     P1IES &= ~SW;                       // Select Interrupt on Rising Edge
     P1IE |= SW;                         // Enable Interrupt on SW pin
 
-    __bis_SR_register(GIE); // Enter LPM4 and Enable CPU Interrupt
+    register_settings_for_TIMER0();
 
-    while(1);
+    while(1)
+    {
+        __bis_SR_register(LPM0_bits +GIE);             // Enable CPU Interrupt
+        switch(clockSource)
+        {
+            case SMCLK: register_settings_for_TIMER0_with_SMCLK();
+                        break;
+
+            case ACLK: register_settings_for_TIMER0_with_ACLK();
+                       break;
+        }
+    }
 }
 
+/*@brief entry point for switch interrupt vector*/
 #pragma vector=PORT1_VECTOR
 __interrupt void Port_1(void)
 {
-    P1OUT ^= GREEN;                     // Toggle Green LED
+    clockSource = !clockSource;
+    __bic_SR_register_on_exit(LPM0_bits + GIE); // Exit LPM0 on return to main
     P1IFG &= ~SW;                       // Clear SW interrupt flag
 }
