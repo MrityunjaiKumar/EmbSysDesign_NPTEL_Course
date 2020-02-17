06399704a1071ea5e39111ec8ceea9b21c6b92f7
Shikhar <pandeyshikhar37@gmail.com>
2020-02-17 19:59:36 +0530

    Updated Hello SSD code

--- Software/Examples_Msp430G2553_LunchBox/Exp12_LunchBox_HelloSSD/Exp12_LunchBox_HelloSSD.c
+++ Software/Examples_Msp430G2553_LunchBox/Exp12_LunchBox_HelloSSD/Exp12_LunchBox_HelloSSD.c
@@ -47,6 +47,8 @@ void main(void) {
     // Initialize 7-segment pins as Output
     P1DIR |= (SEG_A + SEG_B + SEG_C + SEG_D + SEG_E+ SEG_F + SEG_G + SEG_DP);
 
+    P2DIR &= ~SW;                       // Set SW pin -> Input
+
     while(1)
     {
         if(!(P2IN & SW))            // If SW is Pressed
