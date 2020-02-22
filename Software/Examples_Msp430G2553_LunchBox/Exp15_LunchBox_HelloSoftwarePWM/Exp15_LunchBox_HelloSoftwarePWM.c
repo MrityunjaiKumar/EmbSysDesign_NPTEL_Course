8702e1d761ffc429e3e048b006db03abc50ce321
Shikhar <pandeyshikhar37@gmail.com>
2020-02-22 14:16:10 +0530

    Updated Hello Software PWM code

--- Software/Examples_Msp430G2553_LunchBox/Exp15_LunchBox_HelloSoftwarePWM/Exp15_LunchBox_HelloSoftwarePWM.c
+++ Software/Examples_Msp430G2553_LunchBox/Exp15_LunchBox_HelloSoftwarePWM/Exp15_LunchBox_HelloSoftwarePWM.c
@@ -16,26 +16,27 @@ void delay(unsigned int t)          // Custom delay function
 
 /*@brief entry point for the code*/
 void main(void) {
+    unsigned int j;
     WDTCTL = WDTPW | WDTHOLD;           // Stop watchdog timer
 
     P1DIR |= RED;                       // Set LED pin -> Output
 
     while(1)
         {
-            unsigned int j;
-            for(j = 1; j < 500; j++)    // Increasing Intensity
+
+            for(j = 0; j < 256; j++)    // Increasing Intensity
             {
                 P1OUT |= RED;           // LED ON
-                delay(j);               // Delay for ON Time
+                if (j !=0) delay(j);               // Delay for ON Time
                 P1OUT &= ~RED;          // LED OFF
-                delay(500-j);           // OFF Time = Period - ON Time
+                if ((255-j)!=0) delay(255-j);           // OFF Time = Period - ON Time
             }
-            for(j = 500; j > 1; j--)    // Decreasing Intensity
+            for(j = 255; j > 0; j--)    // Decreasing Intensity
             {
                 P1OUT |= RED;           // LED ON
-                delay(j);               // Delay for ON Time
+                if (j !=0) delay(j);               // Delay for ON Time
                 P1OUT &= ~RED;          // LED OFF
-                delay(500-j);           // OFF Time = Period - ON Time
+                if ((255-j) != 0 ) delay(255-j);           // OFF Time = Period - ON Time
             }
         }
 }
