a456cc435725db6a9176d3b1571a6af9f5ac5b8e
divanshudodeja <divanshu.dodeja@gmail.com>
2020-02-25 08:48:43 +0530

    Updated comments in Exp18_LunchBox_HelloSerial.c

--- Software/Examples_Msp430G2553_LunchBox/Exp18_LunchBox_HelloSerial/Exp18_LunchBox_HelloSerial.c
+++ Software/Examples_Msp430G2553_LunchBox/Exp18_LunchBox_HelloSerial/Exp18_LunchBox_HelloSerial.c
@@ -10,9 +10,9 @@ void register_settings_for_UART()
     P1SEL2 = BIT1 + BIT2;
 
     UCA0CTL1 |= UCSSEL_1;               // UART Clock -> ACLK
-    UCA0BR0 = 3;                        // Baud Rate Setting for 1MHz 9600
-    UCA0BR1 = 0;                        // Baud Rate Setting for 1MHz 9600
-    UCA0MCTL = UCBRF_0 + UCBRS_3;       // Modulation Setting for 1MHz 9600
+    UCA0BR0 = 3;                        // Baud Rate Setting for 32Khz 9600
+    UCA0BR1 = 0;                        // Baud Rate Setting for 32Khz 9600
+    UCA0MCTL = UCBRF_0 + UCBRS_3;       // Modulation Setting for 32Khz 9600
     UCA0MCTL &= ~UCOS16;
     UCA0CTL1 &= ~UCSWRST;               // Initialize UART Module
     IE2 |= UCA0RXIE;                    // Enable RX interrupt
